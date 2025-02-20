db51b6047cf2af4d3a1e6d239d1f32be7ee63597
memchr <memchr@proton.me>
2025-02-20 14:01:22 +0000

    feat(editor): add search enhancement plugins

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -44,6 +44,49 @@ return {
     opts = {},
   },
 
+  -- search enhancement
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    vscode = true,
+    ---@type Flash.Config
+    opts = {},
+    enabled = false,
+    -- stylua: ignore
+    keys = {
+      { "<M-f>", mode = { "n" }, function() require("flash").jump() end, desc = "Flash" },
+      {"s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      {"r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<C-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
+
+  -- global search and replace
+  {
+    "MagicDuck/grug-far.nvim",
+    opts = { headerMaxWidth = 80 },
+    cmd = "GrugFar",
+    keys = {
+      {
+        "<leader>sr",
+        function()
+          local grug = require("grug-far")
+          local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+          grug.open({
+            transient = true,
+            prefills = {
+              filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+            },
+          })
+        end,
+        mode = { "n", "v" },
+        desc = "Search and Replace",
+      },
+    },
+  },
+
   {
     "folke/todo-comments.nvim",
     cmd = { "TodoTrouble" },
