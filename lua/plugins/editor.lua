98720b17017e131a4637b17c4b17e72defd0be86
memchr <memchr@proton.me>
2025-02-21 16:16:57 +0000

    feat(editor): add fzf

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -73,6 +73,18 @@ return {
     },
   },
 
+  {
+    "ibhagwan/fzf-lua",
+    init = function()
+      ---@diagnostic disable-next-line
+      vim.ui.select = function(...)
+        require("fzf-lua").register_ui_select()
+        return vim.ui.select(...)
+      end
+    end,
+    opts = {},
+  },
+
   -- search enhancement
   {
     "folke/flash.nvim",
