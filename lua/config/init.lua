2258f14d909a6771ab3ce87b1e219a96ca412813
memchr <memchr@proton.me>
2025-03-06 04:41:45 +0000

    refactor: restructure configs

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,6 +1,11 @@
 local M = {}
+local lazy = require("config.lazy")
 function M.setup()
+  require("config.options")
   require("config.keymap").setup()
+
+  lazy.bootstrap()
+  lazy.setup()
 end
 
 return M
---
+++ lua/config/options.lua
@@ -0,0 +1,37 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- configurations
+local g, opt = vim.g, vim.opt
+
+g.python3_host_prog = "/usr/bin/python3"
+-- disable ruby and repl support
+g.loaded_ruby_provider = 0
+g.loaded_perl_provider = 0
+
+--- enable line number and dedicated sign column
+opt.number = true
+opt.numberwidth = 2
+opt.signcolumn = "yes"
+
+--- use termguicolors
+--- TODO: disable in VT
+opt.termguicolors = true
+
+--- width visual marking
+opt.colorcolumn = "80,120"
+
+--- highlight current line
+opt.cursorline = true
+opt.cursorlineopt = "screenline,number"
+--- enable line warp
+opt.linebreak = true
+
+-- first <TAB> insert the longest common string, second <TAB> complete the next full match
+opt.wildmode = "longest:full,full"
+-- TODO: understand timeout
+
+-- opt.lazyredraw = true
+
+-- TODO: gui
+vim.o.guifont = "Cascadia Code:h11"
