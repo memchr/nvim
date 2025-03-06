f6968a87c3996022bfa6c4fceffe78444be2f73d
memchr <memchr@proton.me>
2025-03-06 04:40:22 +0000

    feat(go): enable semantic highlighting

--- lua/config/lsp/servers.lua
+++ lua/config/lsp/servers.lua
@@ -4,6 +4,24 @@ return {
   clangd = {},
   pyright = {},
   gopls = {
+    settings = {
+      gopls = {
+        semanticTokens = true,
+        analyses = {
+          unusedparams = true,
+        },
+        staticcheck = true,
+        hints = {
+          assignVariableTypes = true,
+          compositeLiteralFields = true,
+          compositeLiteralTypes = true,
+          constantValues = true,
+          functionTypeParameters = true,
+          parameterNames = true,
+          rangeVariableTypes = true,
+        },
+      },
+    },
   },
   rust_analyzer = {},
   lua_ls = {
