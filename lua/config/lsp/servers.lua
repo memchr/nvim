5847edd272bd15dd934228b7a6c2b5925b3a65aa
memchr <memchr@proton.me>
2025-03-07 14:57:33 +0000

    feat(lsp): enable sqls

--- lua/config/lsp/servers.lua
+++ lua/config/lsp/servers.lua
@@ -1,3 +1,6 @@
+local fs = vim.fs
+local uv = vim.uv
+local util = require("lspconfig.util")
 ---@diagnostic disable: missing-fields
 ---@type { [string]:lspconfig.Config }
 return {
@@ -8,4 +11,13 @@ return {
   lua_ls = {},
   bashls = {},
   asm_lsp = {},
+  sqls = {
+    root_dir = util.root_pattern("sqls.yml"),
+    on_new_config = function(config, root_dir)
+      local config_file = fs.joinpath(root_dir, "sqls.yml")
+      if uv.fs_stat(config_file) then
+        config.cmd = { "sqls", "--config", config_file }
+      end
+    end,
+  },
 }
