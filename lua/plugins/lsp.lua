5b49117d31c75ad6c4076c7d572d8e90be85e556
memchr <memchr@proton.me>
2025-03-07 11:22:45 +0000

    fix(lsp): typo in capabilities config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ return {
     config = function(_, opts)
       local lspconfig = require("lspconfig")
       for server, config in pairs(opts.servers) do
-        config.compatibility = require("blink.cmp").get_lsp_capabilities(config.compatibility)
+        config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
         lspconfig[server].setup(config)
       end
     end,
