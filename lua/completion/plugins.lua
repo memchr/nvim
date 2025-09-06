bdc7ad2d54fb08e6a8e75f0494dfc4767a057759
memchr <memchr@proton.me>
2025-09-06 04:17:54 +0000

    refactor(blink): move config

--- lua/completion/plugins.lua
+++ lua/completion/plugins.lua
@@ -1,3 +1,63 @@
+---@module 'blink.cmp'
+---@type blink.cmp.Config
+local blink_opts = {
+  keymap = { preset = "super-tab" },
+  appearance = {
+    -- Fallback to nvi-cmp's highlight groups. compatibility
+    use_nvim_cmp_as_default = true,
+
+    nerd_font_variant = "normal",
+  },
+  completion = {
+    -- Show documentation when selecting a completion item
+    documentation = { auto_show = true, auto_show_delay_ms = 500 },
+
+    menu = {
+      draw = {
+        treesitter = { "lsp" },
+      },
+    },
+    -- Display a preview of the selected item on the current line
+    -- TODO: enable
+    -- ghost_text = { enabled = true },
+  },
+  cmdline = {
+    keymap = {
+      preset = "super-tab",
+      ["<C-p>"] = { "select_prev", "fallback" },
+      ["<C-n>"] = { "select_next", "fallback" },
+    },
+    completion = { menu = { auto_show = true } },
+  },
+  sources = {
+    default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+    providers = {
+      lazydev = {
+        name = "LazyDev",
+        module = "lazydev.integrations.blink",
+        -- make lazydev completions top priority (see `:h blink.cmp`)
+        score_offset = 100,
+      },
+      path = {
+        opts = {
+          show_hidden_files_by_default = true,
+        },
+      },
+    },
+  },
+  fuzzy = {
+    implementation = "rust",
+    prebuilt_binaries = {
+      -- prefer build binary locally
+      download = false,
+    },
+  },
+  -- Experimental signature help support
+  signature = {
+    enabled = true,
+  },
+}
+
 ---@module "lazy"
 ---@type LazySpec[]
 return {
@@ -8,59 +68,6 @@ return {
       "rafamadriz/friendly-snippets",
     },
     event = { "InsertEnter", "CmdlineEnter" },
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      keymap = { preset = "super-tab" },
-      appearance = {
-        -- Fallback to nvi-cmp's highlight groups. compatibility
-        use_nvim_cmp_as_default = true,
-
-        nerd_font_variant = "normal",
-      },
-      completion = {
-        -- Show documentation when selecting a completion item
-        documentation = { auto_show = true, auto_show_delay_ms = 250 },
-
-        menu = {
-          draw = {
-            treesitter = { "lsp" },
-          },
-        },
-        -- Display a preview of the selected item on the current line
-        -- TODO: enable
-        -- ghost_text = { enabled = true },
-      },
-      cmdline = {
-        keymap = {
-          preset = "super-tab",
-          ["<C-p>"] = { "select_prev", "fallback" },
-          ["<C-n>"] = { "select_next", "fallback" },
-        },
-        completion = { menu = { auto_show = true } },
-      },
-      sources = {
-        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-        providers = {
-          lazydev = {
-            name = "LazyDev",
-            module = "lazydev.integrations.blink",
-            -- make lazydev completions top priority (see `:h blink.cmp`)
-            score_offset = 100,
-          },
-        },
-      },
-      fuzzy = {
-        implementation = "rust",
-        prebuilt_binaries = {
-          -- prefer build binary locally
-          download = false,
-        },
-      },
-      -- Experimental signature help support
-      signature = {
-        enabled = true,
-      },
-    },
+    opts = blink_opts,
   },
 }
