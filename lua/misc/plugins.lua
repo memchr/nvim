1cf30eadb3d6990281bf70d461cd757b96159e94
memchr <memchr@proton.me>
2025-10-03 10:00:43 +0000

    feat: add zk-nvim

--- lua/misc/plugins.lua
+++ lua/misc/plugins.lua
@@ -2,4 +2,10 @@
 return {
   { "nfnty/vim-nftables" },
   { "nvim-lua/plenary.nvim", lazy = true },
+  {
+    "zk-org/zk-nvim",
+    config = function()
+      require("zk").setup()
+    end,
+  },
 }
